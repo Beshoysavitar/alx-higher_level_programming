aec27192c599333dece708738f2b633656d195cf
beshoy <besho.elking33377@gmail.com>
2023-09-13 18:12:01 -0700

    python

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    mv = 0
-    mk = None
-    for x, i in a_dictionary.items():
-        if i > maxval:
-            mv = i
-            mk = x
-    return mk
+    if a_dictionary and len(a_dictionary):
+        max = list(a_dictionary.keys())[0]
+        for key in a_dictionary:
+            if a_dictionary[key] > a_dictionary[max]:
+                max = key
+        return max
+    return None
