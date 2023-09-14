be895b13a0bc95f347dce85d842e05bb66be4c94
beshoy <besho.elking33377@gmail.com>
2023-09-13 18:09:05 -0700

    python

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,11 @@
+#!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary:
-        return max(a_dictionary, key=a_dictionary.get)
-    return None
+    if a_dictionary is None:
+        return None
+    mv = 0
+    mk = None
+    for x, i in a_dictionary.items():
+        if i > maxval:
+            mv = i
+            mk = x
+    return mk
