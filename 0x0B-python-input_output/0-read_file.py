9ca92f5dbec12cd186e0156c2523c8d1894a2834
beshoy <besho.elking33377@gmail.com>
2024-02-20 17:31:50 -0800

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,7 +3,10 @@
 read file module
 """
 def read_file(filename=""):
-    """""reads a text file(UTF8) and prints it to stdout"""
+    """read_file
+    args:
+        filename: string
+    """
     with open(filename, encoding="utf-8") as file:
         read_data = file.read()
         print(read_data, end="")
