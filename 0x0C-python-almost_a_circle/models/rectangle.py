d9a9f8cfefd699298e6eb19d8e22dece46315a87
beshoy <besho.elking33377@gmail.com>
2024-02-26 11:57:41 -0800

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-'''rectangle class module'''
+'''Module for Rectangle class.'''
 from models.base import Base
 
 
 class Rectangle(Base):
-    '''rectangle class module'''
+    '''A Rectangle class.'''
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        """inint
+        '''inint
         args:
             width: int
             hight: int
             x: int
-            y: int
-        """
+            y: int'''
         super().__init__(id)
         self.width = width
         self.height = height
@@ -21,50 +21,50 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        '''rectangle class module'''
+        '''Width of this rectangle.'''
         return self.__width
 
     @width.setter
     def width(self, value):
-        '''rectangle class module'''
+        '''width setter'''
         self.validate_integer("width", value, False)
         self.__width = value
 
     @property
     def height(self):
-        '''rectangle class module'''
+        '''Height of this rectangle.'''
         return self.__height
 
     @height.setter
     def height(self, value):
-        '''rectangle class module'''
+        '''height setter'''
         self.validate_integer("height", value, False)
         self.__height = value
 
     @property
     def x(self):
-        '''rectangle class module'''
+        '''x of this rectangle.'''
         return self.__x
 
     @x.setter
     def x(self, value):
-        '''rectangle class module'''
+        '''x setter'''
         self.validate_integer("x", value)
         self.__x = value
 
     @property
     def y(self):
-        '''rectangle class module'''
+        '''y of this rectangle.'''
         return self.__y
 
     @y.setter
     def y(self, value):
-        '''rectangle class module'''
+        '''y setter'''
         self.validate_integer("y", value)
         self.__y = value
 
     def validate_integer(self, name, value, eq=True):
-        '''rectangle class module'''
+        '''Method for validating the value.'''
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if eq and value < 0:
@@ -73,23 +73,23 @@ class Rectangle(Base):
             raise ValueError("{} must be > 0".format(name))
 
     def area(self):
-        '''rectangle class module'''
+        '''Computes area of this rectangle.'''
         return self.width * self.height
 
     def display(self):
-        '''rectangle class module'''
+        '''Prints string representation of this rectangle.'''
         s = '\n' * self.y + \
             (' ' * self.x + '#' * self.width + '\n') * self.height
         print(s, end='')
 
     def __str__(self):
-        '''rectangle class module'''
+        '''Returns string info about this rectangle.'''
         return '[{}] ({}) {}/{} - {}/{}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width,
                    self.height)
 
     def __update(self, id=None, width=None, height=None, x=None, y=None):
-        '''rectangle class module'''
+        '''Internal method that updates instance attributes via */**args.'''
         if id is not None:
             self.id = id
         if width is not None:
@@ -102,14 +102,14 @@ class Rectangle(Base):
             self.y = y
 
     def update(self, *args, **kwargs):
-        '''rectangle class module'''
-        #print(args, kwargs)
+        '''Updates instance attributes via no-keyword & keyword args.'''
+        # print(args, kwargs)
         if args:
             self.__update(*args)
         elif kwargs:
             self.__update(**kwargs)
 
     def to_dictionary(self):
-        '''rectangle class module'''
+        '''Returns dictionary representation of this class.'''
         return {"id": self.id, "width": self.__width, "height": self.__height,
                 "x": self.__x, "y": self.__y}
