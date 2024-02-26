8b01e9ef94b3b1937fc20ae5459c92587d785712
beshoy <besho.elking33377@gmail.com>
2024-02-26 12:32:05 -0800

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,51 +1,173 @@
 #!/usr/bin/python3
-"""Module with class Square that inherits from Rectangle"""
-from models.rectangle import Rectangle
+"""Defines a class Rectangle that inherits from Base"""
+from models.base import Base
 
 
-class Square(Rectangle):
-    """Class Square that inherits from Rectangle
+class Rectangle(Base):
+    """Class that defines properties of Rectangle.
 
-    Attributes:
-        size (int): size of a side of the square
-        x (int): x coordinate of the square
-        y (int): y coordinate of the square
-        id (int): identity of the square
+     Attributes:
+        width (int): width of rectangle.
+        height (int): height of rectangle.
+        x (int): x.
+        y (int): y.
     """
-
-    def __init__(self, size, x=0, y=0, id=None):
-        """Initializes a Square
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Creates new instances of rectangle.
 
         Args:
-            size (int): size of a side of the square
-            x (int, optional): x coordinate. Defaults to 0
-            y (int, optional): y coordinate. Defaults to 0
-            id (int, optional): defaults to None
+            width (int): width of rectangle.
+            height (int): height of rectangle.
+            x (int, optional): x. Defaults to 0.
+            y (int, optional): y. Defaults to 0.
+            id (int, optional): Identity number of rectangle. Defaults to None.
         """
-        super().__init__(size, size, x, y, id)
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
-    def size(self):
-        """Getter of the size
+    def width(self):
+        """Width retriever.
 
         Returns:
-            int: size of the square
+            int: width of rectangle.
         """
-        return self.width
+        return self.__width
 
-    @size.setter
-    def size(self, value):
-        """Setter of the size
+    @width.setter
+    def width(self, value):
+        """Property setter for width of rectangle.
 
         Args:
-            value (int): new size of the square
+            value (int): width of rectangle.
 
         Raises:
-            TypeError: if value is not an int
-            ValueError: if value <= 0
+            TypeError: if width is not an integer.
+            ValueError: if width is less than or equal to zero.
         """
-        self.width = value
-        self.height = value
+        self.validate_integer("width", value, False)
+        self.__width = value
+
+    @property
+    def height(self):
+        """Height retriever.
+
+        Returns:
+            int: height of rectangle.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Property setter for height of rectangle.
+
+        Args:
+            value (int): height of rectangle.
+
+        Raises:
+            TypeError: if height is not an integer.
+            ValueError: if height is less than or equal to zero.
+        """
+        self.validate_integer("height", value, False)
+        self.__height = value
+
+    @property
+    def x(self):
+        """x retriever.
+
+        Returns:
+            int: x.
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Property setter for x.
+
+        Args:
+            value (int): x.
+
+        Raises:
+            TypeError: if x is not an integer.
+            ValueError: if x is less than or equal to zero.
+        """
+        self.validate_integer("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        """y retriever.
+
+        Returns:
+            int: y.
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Property setter for y.
+
+        Args:
+            value (int): y.
+
+        Raises:
+            TypeError: if y is not an integer.
+            ValueError: if y is less than or equal to zero.
+        """
+        self.validate_integer("y", value)
+        self.__y = value
+
+    def validate_integer(self, name, value, eq=True):
+        """Method for validating the value."""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if eq and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+        elif not eq and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+
+    def area(self):
+        """Calculates area of a rectangle.
+
+        Returns:
+            int: area.
+        """
+        return self.width * self.height
+
+    def display(self):
+        """Prints in stdout #."""
+        s = '\n' * self.y + \
+            (' ' * self.x + '#' * self.width + '\n') * self.height
+        print(s, end='')
+
+    def __str__(self):
+        """Returns string rectangle."""
+        return '[{}] ({}) {}/{} - {}/{}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width,
+                   self.height)
+
+    def __update(self, id=None, width=None, height=None, x=None, y=None):
+        """Class that defines properties of Rectangle.
+
+        Attributes:
+            width (int): width of rectangle.
+            height (int): height of rectangle.
+            x (int): x.
+            y (int): y.
+        """
+        if id is not None:
+            self.id = id
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
 
     def update(self, *args, **kwargs):
         """Update attributes of the square
@@ -58,35 +180,17 @@ class Square(Rectangle):
                 - 4th: y attribute
             **kwargs (dict): new key/value pairs of attributes
         """
-        if args and len(args) != 0:
-            attrs = ["id", "size", "x", "y"]
-            for i, a in enumerate(args):
-                setattr(self, attrs[i], a)
-
-        elif kwargs and len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k in ["id", "size", "x", "y"]:
-                    setattr(self, k, v)
+        # print(args, kwargs)
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
 
     def to_dictionary(self):
-        """Returns dictionary representation of the square
+        """Returns the dictionary representation of a Rectangle.
 
         Returns:
-            dict: dictionary representation
+            dict: rectangle.
         """
-        d = {}
-        d["id"] = self.id
-        d["size"] = self.size
-        d["x"] = self.x
-        d["y"] = self.y
-
-        return d
-
-    def __str__(self):
-        """Return string representation"""
-        return "[{}] ({}) {}/{} - {}".format(
-            type(self).__name__,
-            self.id,
-            self.x,
-            self.y,
-            self.size)
+        return {"id": self.id, "width": self.__width, "height": self.__height,
+                "x": self.__x, "y": self.__y}
